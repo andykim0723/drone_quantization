7e919de22cdea037cd2f583db59d71a03afa1d84
andykim0723 <styx2323@naver.com>
2024-08-22 17:17:44 +0900

    update: VisDrone_custom.yaml

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO('./weights/yolov8l.pt', task='detect')
 
-model.train(data='./cfgs/VisDrone_custom.yaml', epochs=100, imgsz=640, device='0', batch=24, optimizer='Adam', 
-         weight_decay=1e-5)
+# model.train(data='./cfgs/VisDrone_custom.yaml', epochs=100, imgsz=640, device='0', batch=24, optimizer='Adam', 
+#          weight_decay=1e-5)
+model.train(data='./cfgs/VisDrone_custom.yaml', epochs=100, imgsz=640, device='0, 1', batch=64, optimizer='Adam', 
+         weight_decay=5e-6)
